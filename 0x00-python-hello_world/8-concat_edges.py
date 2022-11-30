76c96cb47b68a61c4e1640af92526004f2415381
= <=>
2022-11-30 13:20:17 -0800

    python loops

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
+str = "Python is an interpreted, interactive, object-oriented programming \
 language that combines remarkable power with very clear syntax"
-str = "object-oriented programming with Python"
+str = str.split(' ')
+str = str[5] + ' ' + str[6] +  ' ' + str[12] + ' ' + str[0]
 print(str)

3907038111461d2daf89679ac36e64d0fdabc524
= <=>
2022-11-29 18:14:14 -0800

    python coding style

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,12 +2,14 @@
 import random
 number = random.randint(-10000, 10000)
 if (number > 0):
-	last_digit = int(str(number)[-1])
+    last_digit = int(str(number)[-1])
 if (number < 0):
-	last_digit = int('-' + str(number)[-1])
+    last_digit = int('-' + str(number)[-1])
+if number == 0:
+    last_digit = 0
 if (last_digit > 5):
-	print(f"Last digit of {number} is {last_digit} and is greater than 5")
+    print(f"Last digit of {number} is {last_digit} and is greater than 5")
 elif (last_digit == 0):
-	print(f"Last digit of {number} is {last_digit} and is 0")
+    print(f"Last digit of {number} is {last_digit} and is 0")
 elif (last_digit < 6 and last_digit != 0):
-	print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
+    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")

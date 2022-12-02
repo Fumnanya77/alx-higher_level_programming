5840558a72fdf339516411c7f372c65369359f9c
= <=>
2022-12-01 16:53:00 -0800

    import modules

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+
+argv_list = sys.argv
+
+if len(argv_list) == 1:
+    print(f"0 arguments")
+if len(argv_list) == 2:
+    print(f"1 argument:")
+if len(argv_list) > 2:
+    print(f"{len(argv_list)-1} arguments:")
+
+for argu in range(0, len(argv_list)):
+    if argu == 0:
+        continue
+    print(f"{argu}: {argv_list[argu]}")
---
+++ 0x02-python-import_modules/README.md
@@ -0,0 +1,1 @@
+### Directory on Python Import Modules

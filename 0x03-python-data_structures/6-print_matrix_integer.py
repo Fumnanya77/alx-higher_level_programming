7da0e9c0ac01f5f283ff16d3999f393a5de6a522
= <=>
2022-12-05 10:30:11 -0800

    list and omission

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for col in row:
-            print("{:2d}".format(col), end=""),
+            print(f"{:2d}".format(col), end=""),
         print()

c2f3c071312af4c76ab7adf331ecdfb03fc6c66a
= <=>
2022-12-05 16:19:34 -0800

    fixing list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,4 +2,4 @@
 def print_reversed_list_integer(my_list=[]):
     my_list.reverse()
     for i in range(len(my_list)):
-        print(f"{my_list[i]:d}")
+        print("{:d}".format(my_list[i]))
--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    tuple_c = ()
-    tuple_c[0] = tuple_a[0] + tuple_b[0]
-    tuple_c[1] = tuple_a[1] + tuple_b[1]
+    tuple_c = tuple(map(sum,zip(tuple_a, tuple_b)))
+	if len(tuple_a) >= 2 or len(tuple_b) >= 2:
+        
     return tuple_c

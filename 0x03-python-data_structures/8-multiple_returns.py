7f10d68f750e512412469fb6949a08a140933961
= <=>
2022-12-06 16:50:52 -0800

    response to checker on lists and tuples

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    lent = len(sentence)
     firs = sentence[0]
     if sentence == "":
         lent = 0
         firs = None
+    else:
+        lent = len(sentence)
+        firs = firs
     return (lent, firs)
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     temp = 0
+    if len(my_list) == 0:
+        temp = None
     for i in range(len(my_list)):
         if my_list[i] > temp:
             temp = my_list[i]
-        else:
-            continue
-        if my_list[i] < 0 and my_list[i+1] < 0 and temp <= 0:
+        elif my_list[i] < 0 and my_list[i+1] < 0 and temp <= 0:
             if my_list[i] < temp:
                 temp = my_list[i]
-    if my_list == []:
-        temp = None
+        else:
+            continue
     return temp

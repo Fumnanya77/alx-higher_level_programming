1760c3495c5019b707006f6afd58920bb352e825
= <=>
2023-01-09 08:32:27 -0800

    An object attributes and methods

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# python3 -c 'print(__import__("my_module").__doc__)'
+# python3 -c 'print(__import__("0-lookup").lookup.__doc__)'
 def lookup(obj):
     lup = print(dir(obj))
     return lup

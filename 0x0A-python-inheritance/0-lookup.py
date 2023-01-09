fd91a6deb721810c053eccf48d54099eb8ae999a
= <=>
2023-01-09 09:12:58 -0800

    proper documentation

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-# python3 -c 'print(__import__("0-lookup").lookup.__doc__)'
+"""python3 -c 'print(__import__("0-lookup").lookup.__doc__)'
+
+    This fuction prints out the attributes and methods of an obect
+"""
+
 def lookup(obj):
     lup = print(dir(obj))
     return lup

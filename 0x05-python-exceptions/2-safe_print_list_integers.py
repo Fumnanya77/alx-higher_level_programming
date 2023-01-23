06ebbb83889a3b5f37faea62acc2e352c1a84b1a
= <=>
2023-01-23 10:04:17 -0800

    Print and count integers

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,7 +3,7 @@ def safe_print_list_integers(my_list=[], x=0):
     counter = 0
     try:
         for i in range(x):
-            if isinstance(my_list[i], int):                
+            if isinstance(my_list[i], int):
                 print("{}".format(my_list[i]), end="")
                 counter = counter + 1
             else:

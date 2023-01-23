2097f76ac2cde0912262670bcaed9081c749e796
= <=>
2023-01-23 08:24:58 -0800

    Safe printing of integers list

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,7 +3,6 @@ def safe_print_integer(value):
     try:
         int(value)
         print("{:d}".format(value))
-    except ValueError:
-        return False
-    else:
         return True
+    except (ValueError, TypeError):
+        return False

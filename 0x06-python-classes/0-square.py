dfcaeb88e8c664912d3b21ace8771272b245b317
= <=>
2023-01-24 13:38:01 -0800

    Initial commit of objects and classes: my first square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """ This module is about making squares """
+
+
 class Square:
     """ The square class
-			
+
         The square class creates attributes and methods for performing \
         and performing arithemetic with squares
     """

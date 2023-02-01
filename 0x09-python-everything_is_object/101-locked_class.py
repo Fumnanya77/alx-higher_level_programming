3f317a212b6456a359895993455fe1b668cdc8d9
= <=>
2023-02-01 07:54:45 -0800

    low memory cost

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+"""Creating a locked class.
+
+Creation of a class that restricts the creation of new instance attributes
+
+"""
+
+
 class LockedClass():
+    """ The locked class"""
     __slots__ = ['first_name']
 
     def __init__(self):
+        """ the instance method """
         first_name = ""

6ffb2fae2cfb874e15c68ecfdbb0c7af81bba914
= <=>
2023-02-11 06:33:00 -0800

    integer addition

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" This module is about adding two integers
+
+    It should be explicit on what to add and not add.
+"""
+
+
+def add_integer(a, b=98):
+    """ This function adds two floats or integers """
+
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    else:
+        a = int(a)
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    else:
+        b = int(b)
+    return(a + b)
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,5 @@
+## TEST-DRIVEN DEVELOPMENT
+
+### Project 0x07-python-test_driven_development
+
+	To test codes to make sure they do what is intended

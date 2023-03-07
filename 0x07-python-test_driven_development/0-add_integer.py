f2ee09cabb0b62648cfb44a1c3bfa4c258fab502
= <=>
2023-03-07 08:24:29 -0800

    testing

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -8,12 +8,15 @@
 def add_integer(a, b=98):
     """ This function adds two floats or integers """
 
-    if not isinstance(a, (int, float)):
-        raise TypeError("a must be an integer")
+    if a or b:
+        if not isinstance(a, (int, float)):
+            raise TypeError("a must be an integer")
+        else:
+            a = int(a)
+        if not isinstance(b, (int, float)):
+            raise TypeError("b must be an integer")
+        else:
+            b = int(b)
     else:
-        a = int(a)
-    if not isinstance(b, (int, float)):
-        raise TypeError("b must be an integer")
-    else:
-        b = int(b)
+        raise TypeError("add_integer() missing 1 required positional argument: 'a'")
     return(a + b)

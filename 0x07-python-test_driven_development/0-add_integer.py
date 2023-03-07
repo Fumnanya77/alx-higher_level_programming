d3044c5ac97ab35e8125d5f6822ad303a0eb2751
= <=>
2023-03-07 10:02:02 -0800

    testing

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -18,5 +18,5 @@ def add_integer(a, b=98):
         else:
             b = int(b)
     else:
-        raise TypeError("add_integer() missing 1 required argument: 'a'")
+        raise TypeError("add_integer() missing 1 required positional argument: 'a'")
     return(a + b)
